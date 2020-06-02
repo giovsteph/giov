48e907259ab3de80f66f09bcfa5ac5705b944df0
giovsteph <giov.ponce@gmail.com>
2020-06-02 12:56:17 -0500

    :bento: adding reference by Jay

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyD0J9RKkT-nhXwftLfsg4mIXukYROuVUAA",
+      authDomain: "devgioportfolio.firebaseapp.com",
+      databaseURL: "https://devgioportfolio.firebaseio.com",
+      projectId: "devgioportfolio",
+      storageBucket: "devgioportfolio.appspot.com",
+      messagingSenderId: "394062323934",
+      appId: "1:394062323934:web:3796fd3a189467ba1ee24f"
+  }
 };
