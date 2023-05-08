7ed1bdc31c1a78f539f6ad94fbcf6d10ae4f85fc
Giovanna Stephany Ponce Castillo <giov.ponce@gmail.com>
2023-05-08 14:43:24 -0600

    feat: added fonts to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import "./index.css";
+
 import MainPage from "./components/Pages/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <p>hello</p>
     </div>
   );
 }
