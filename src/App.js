916e9e70d72552e759a36b62992408e1057cf765
Giovanna Stephany Ponce Castillo <giov.ponce@gmail.com>
2023-05-23 20:25:12 -0600

    feat: scrollbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import "./index.css";
+import ScrollableContainer from "./components/Atoms/ScrollableContainer";
 
 import MainPage from "./components/Pages/MainPage/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <ScrollableContainer>
+      <div className="App">
+        <MainPage />
+      </div>
+    </ScrollableContainer>
   );
 }
 
