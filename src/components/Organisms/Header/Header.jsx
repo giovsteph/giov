f180360376a8e00a9f4151ef5df972ff816ce6c9
Giovanna Stephany Ponce Castillo <giov.ponce@gmail.com>
2023-05-23 20:04:16 -0600

    feat: menu open-close animation - removed scroll

--- src/components/Organisms/Header/Header.jsx
+++ src/components/Organisms/Header/Header.jsx
@@ -31,35 +31,103 @@ function useScrollDirection() {
 }
 const Header = () => {
   const scrollDirection = useScrollDirection();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+    document.body.classList.toggle("disable-scroll");
+  };
+
+  function MobileView() {
+    return (
+      <div className="header">
+        <div className="icon-container">
+          <a href="/">
+            <img src={SkullLogo} alt="logo"></img>
+          </a>
+        </div>
+        <div className="menuContainer">
+          <div
+            className={`hamburgerMenu ${isMenuOpen ? "open" : "closed"}`}
+            onClick={handleMenuToggle}
+          >
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </div>
+        {isMenuOpen && (
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              <li className="mobile-nav-item">
+                01. <Link href="#about">About</Link>
+              </li>
+              <li className="mobile-nav-item">
+                02. <Link href="#experience">Experience</Link>
+              </li>
+              <li className="mobile-nav-item">
+                03. <Link href="#work">Work</Link>
+              </li>
+              <li className="mobile-nav-item">
+                04. <Link href="#contact">Contact</Link>
+              </li>
+              <li className="mobile-nav-item">
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                  <Button className="primary smaller">Resume</Button>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
+    );
+  }
+
+  function DesktopView() {
+    return (
+      <div className="header">
+        <div className="icon-container">
+          <a href="/">
+            <img src={SkullLogo} alt="logo"></img>
+          </a>
+        </div>
+        <nav className="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              01. <Link href="#about">About</Link>
+            </li>
+            <li className="nav-item">
+              02. <Link href="#experience">Experience</Link>
+            </li>
+            <li className="nav-item">
+              03. <Link href="#work">Work</Link>
+            </li>
+            <li className="nav-item">
+              04. <Link href="#contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="btnContainer">
+            <a href="/resume.pdf" target="_blank">
+              <Button className="primary smaller">Resume</Button>
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 
   return (
     <div className={`header ${scrollDirection === "down" ? "hide" : ""}`}>
-      <div className="icon-container">
-        <a href="/">
-          <img src={SkullLogo} alt="logo"></img>
-        </a>
-      </div>
-      <nav className="navbar">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            01. <Link href="#about">About</Link>
-          </li>
-          <li className="nav-item">
-            02. <Link href="#experience">Experience</Link>
-          </li>
-          <li className="nav-item">
-            03. <Link href="#work">Work</Link>
-          </li>
-          <li className="nav-item">
-            04. <Link href="#contact">Contact</Link>
-          </li>
-        </ul>
-        <div className="btnContainer">
-          <a href="/resume.pdf" target="_blank">
-            <Button className="primary smaller">Resume</Button>
-          </a>
-        </div>
-      </nav>
+      {windowWidth <= 768 ? <MobileView /> : <DesktopView />}
     </div>
   );
 };
